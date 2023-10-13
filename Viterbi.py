c54d668213eeff4c7adea9435e5e5013dd2539b6
zcythan <jferrick@umich.edu>
2023-10-12 20:44:32 -0400

    Some of the prediction logic implemented

--- Viterbi.py
+++ Viterbi.py
@@ -25,17 +25,42 @@ class Viterbi:
         self.__tagsForWords = self.__getTagsForWords()
 
     #Calculates the probability a tag starts a sentence,
-    #as there's no beginning of sentence tag in the data.
+    #as there's no beginning of sentence tag in the data...
     def __tagStarts(self, tag):
-        return self.__startTagCounts.get(tag, -1) / self.__lineCount
+        if self.__lineCount > 0:
+            return self.__startTagCounts.get(tag, 0) / self.__lineCount
+        return 0
 
     #Calculates the probability of a tag given a tag.
     def __tagGivenTag(self, tag, prevTag):
-        return self.tagForTagCounts.get(tag+prevTag, -1)/self.__tagCounts.get(prevTag)
+        return self.tagForTagCounts.get(tag+prevTag, 0)/self.__tagCounts.get(prevTag, 0)
 
     # Calculates the probability of word given a tag
     def __wordGivenTag(self, word, tag):
-        return self.__wordTagCounts.get(word+tag, -1)/self.__tagCounts.get(tag, -1)
+        return self.__wordTagCounts.get(word+tag, 0)/self.__tagCounts.get(tag, 0)
+
+    def predict(self, file):
+        with open(self.__file, 'r') as data:
+            for line in data:
+                init = True
+                score = {}
+                backPtr = {}
+                words = line.split()
+                for part in words:
+                    if '/' in part:
+                        full = self.__removeExtra(part)
+                        # First half cast to lowercase, reduce duplicates.
+                        word = tag[0:full.rfind('/')].lower()
+                        #tag = tag[tag.rfind('/'):]
+                        #Initialization
+                        if init:
+                            potTags = self.__tagsForWords.get(word, 0)
+                            for tag in potTags:
+                                score[0].append(tag, self.__wordGivenTag(word, tag) * self.__tagStarts(tag))
+                                backPtr = 0
+
+                            init = False
+
 
     #Makes list of tags a word has been seen as.
     def __getTagsForWords(self):
@@ -45,7 +70,7 @@ class Viterbi:
             word, tag = elem.split('/')
             if word not in wordTagDict:
                 wordTagDict[word] = []
-            # Leaving the / makes things easier later on due to how my wordTagCounts works.
+            # Leaving the / makes things easier later on.
             wordTagDict[word].append('/' + tag)
         return wordTagDict
 
@@ -77,7 +102,6 @@ class Viterbi:
         with open(self.__file, 'r') as inp:
             for line in inp:
                 lineCount += 1
-                #Using /BEG to notate beginning of sentence
                 prevTag = ""
                 words = line.split()
                 for word in words:
